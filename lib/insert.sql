7da0fb8d2c9712e2e5319fb25f532e239a814222
Marc Poppy <poppy.marc@gmail.com>
2019-09-16 03:53:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,9 @@
 
 INSERT INTO bears(name) VALUES ("Mr. Chocolate");
+INSERT INTO bears(name) VALUES ("Rowdy");
+INSERT INTO bears(name) VALUES ("Mr. Chocolate");
+INSERT INTO bears(name) VALUES ("Mr. Chocolate");
+INSERT INTO bears(name) VALUES ("Mr. Chocolate");
+INSERT INTO bears(name) VALUES ("Mr. Chocolate");
+INSERT INTO bears(name) VALUES ("Mr. Chocolate");
+INSERT INTO bears(name) VALUES ("Mr. Chocolate");
